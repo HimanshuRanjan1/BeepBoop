828eff01c847e86d666a3e9b5c0aeb8028db7049
RanjanH <himanshu02ranjan@gmail.com>
2023-09-15 00:49:45 +0530

    song commands

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import dotenv
 import discord
 from PIL import Image, ImageDraw
 import random
+import yt_dlp as youtube_dl
+import asyncio
+from termcolor import colored
+
 
 #Photo Edit
 
@@ -21,12 +25,52 @@ def add_corners(im, rad):
     im.putalpha(alpha)
     return im
 
+#youtube-dl initializing
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # binding to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
 #env file loader
 
 dotenv.load_dotenv()
 
 token = os.getenv("DISCORD_TOKEN")
-guilds = os.getenv("DISCORD_GUILD")
+guilds = {}
 
 #Bot initiator
 
@@ -38,31 +82,46 @@ cmd = discord.app_commands.CommandTree(bot)
 chatWith = ""
 knMsg = 0
 
-#VoiceClient
-vClient = None
 
 #Bot Events
+
+def register(guild):
+    file = open("guilds.txt",'a')
+    file.write(guild.name+',')
+    file.close()
 
 
 @bot.event
 async def on_ready():
-    guild_count = 0
-    await cmd.sync(guild = discord.Object(id = 1149408094711980172))
+    global guilds
 
+    file = open("guilds.txt",'r')
+    file_txt = []
+    name = ''
+    for i in file:
+        if i != ',':
+            name += i
+        else:
+            file_txt.append(name)
+            name = ''
+    file.close()
     for guild in bot.guilds:
-        if guild.name == guilds:
-            print(f"- {guild.id} (name : {guild.name})\n")
-            break
-
-        guild_count += 1
+        guilds[guild.name] = 0
+        if guild.name not in file_txt:
+            register(guild)
+        await cmd.sync(guild = discord.Object(id = guild.id))
         
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f"Guild Members : \n - {members}")
+    print(colored("Boop is READY!",attrs = ['bold']))
     
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1149408095672483945)
+    try:
+        for ch in member.guild.channels:
+            if ch.name.lower() == 'welcome':
+                channel = bot.get_channel(ch.id)
+    except:
+        channel = bot.get_channel((member.guild.channels)[0].id)
     welcome = {"wild":f"Huh!! A wild {member.mention} appeared","pizza":f"Welcome, {member.mention}. We hope you brought pizza.","weapon":f"Welcome, {member.mention}. Leave your weapons by the door.","banana":f"{member.mention} just joined. HIDE YOUR BANANAS!","spawn":f"{member.mention} has spawned in the guild","challenge":f"Challenger approaching - {member.mention} has appeared!","disappoint":f"It's a bird! It's a plane! Nevermind, it's just {member.mention}."}
     choice = random.choice(list(welcome.keys()))
     pics = {"pizza":"img\\pizza.png","weapon":"img\\weapons.png","banana":"img\\banana.png","spawn":"img\\spawn.gif","challenge":"img\\vs.png","disappoint":"img\\disappointed.png"}
@@ -87,27 +146,26 @@ async def on_member_join(member):
 @bot.event
 async def on_message(message):
     global chatWith,knMsg
-    channel = bot.get_channel(1149408183786418317)
+    channel = bot.get_channel(message.channel.id)
     greet = ["What's kicking, little chicken?","Howdy, partner!","Wassup, homey?","Tring tring…this chat may or may not be recorded for training purposes."]
     if message.author == bot.user:
         return
 
-    if message.channel.name == "general":
-        if message.content.lower() == "hello boop":
-            await channel.send(random.choice(greet))
+    if message.content.lower() == "hello boop":
+        await channel.send(random.choice(greet))
 
-        elif message.content.lower() == "knock knock":
-            chatWith = message.author
+    elif message.content.lower() == "knock knock":
+        chatWith = message.author
+        knMsg = knMsg + 1
+        await channel.send("Who's There?")
+
+    elif message.author == chatWith:
+        if knMsg == 1:
+            await channel.send(f"{message.content} who?")
             knMsg = knMsg + 1
-            await channel.send("Who's There?")
-
-        elif message.author == chatWith:
-            if knMsg == 1:
-                await channel.send(f"{message.content} who?")
-                knMsg = knMsg + 1
-            elif knMsg == 2:
-                react = ["LMAO 😂","I am Speechless! You are so Hilarious! 😂"]
-                await channel.send(random.choice(react))
+        elif knMsg == 2:
+            react = ["LMAO 😂","I am Speechless! You are so Hilarious! 😂"]
+            await channel.send(random.choice(react))
 
 #Bot Commands
 
@@ -122,44 +180,101 @@ async def say(args,string: str):
 
 @cmd.command(name = "join_voice",description = "Prompts Boop to join a voice channel",guild = discord.Object(id = 1149408094711980172))
 async def join(args,string: str):
-    global vClient
+    global guilds
     try:
-        if vClient.is_connected():
-            await args.response.send_message(f"Boop is already in Channel {vClient.channel.name}.")
-            await args.channel.send("Try using '/hop_to' command to change the Channel Boop is Connected to.")
-    except:
         for vc in bot.get_guild(args.guild_id).voice_channels:
             if vc.name.lower() == string.lower():
                 break
         channel = bot.get_channel(vc.id)
-        vClient = await channel.connect()
+        guilds[args.guild.name] = await channel.connect()
         await args.response.send_message(f"Boop Connected to Channel {vc.name}.")
+    except:
+        await args.response.send_message(f"Boop is already in Channel {vClient.channel.name}.")
+        await args.channel.send("Try using '/hop_to' command to change the Channel Boop is Connected to.")
+
 
 @cmd.command(name = "leave_voice",description = "Prompts Boop to leave the Voice Channel",guild = discord.Object(id = 1149408094711980172))
 async def leave(args):
-    global vClient
+    vClient = args.guild.voice_client
     try:
         await vClient.disconnect()
         await args.response.send_message(f"Boop Disconnected from Channel {vClient.channel.name}.")
     except:
         await args.response.send_message(f"Boop is not Connected to any Voice Channel.")
 
+
 @cmd.command(name = "hop_to",description = "Prompts Boop to hop to another Voice Channel",guild = discord.Object(id = 1149408094711980172))
 async def hop(args,string: str):
-    global vClient
+    global guilds
+    vClient = args.guild.voice_client
     try:
-        if not vClient.is_connected():
-            await args.response.send_message(f"Boop is not Connected to any Channel.")
-            await args.channel.send("Try using '/join_voice' command to prompt Boop to Connect to a Channel.")
-    except:
         await vClient.disconnect()
         for vc in bot.get_guild(args.guild_id).voice_channels:
             if vc.name.lower() == string.lower():
                 break
         channel = bot.get_channel(vc.id)
-        vClient = await channel.connect()
+        guilds[args.guild.name] = await channel.connect()
         await args.response.send_message(f"Boop Hopped to Voice Channel {vc.name}.")
+    except:
+        await args.response.send_message(f"Boop is not Connected to any Channel.")
+        await args.channel.send("Try using '/join_voice' command to prompt Boop to Connect to a Channel.")
 
+
+@cmd.command(name = "play_song",description = 'Prompts Boop to play the mentioned song',guild = discord.Object(id = 1149408094711980172))
+async def play(args,url: str):
+    global guilds
+    vClient = guilds[args.guild.name]
+    try:
+        vc = bot.get_guild(args.guild_id).voice_channels[0]
+        channel = bot.get_channel(vc.id)
+        guilds[args.guild.name] = await channel.connect()
+        vClient = guilds[args.guild.name]
+    except:
+        pass
+
+    await args.channel.typing()
+    await args.response.send_message(f"**Boop is Now Playing the song **")
+
+    filename = await YTDLSource.from_url(url,loop = bot.loop)
+    vClient.play(discord.FFmpegPCMAudio(executable = "C:\\Personal Data\\Codes\\ffmpeg\\bin\\ffmpeg.exe",source = filename))
+
+
+@cmd.command(name = "pause_song",description = "Prompts Boop to pause the song its playing",guild = discord.Object(id = 1149408094711980172))
+async def pause(args):
+    global guilds
+    vClient = guilds[args.guild.name]
+    await args.channel.typing()
+    try:
+        vClient.pause()
+        await args.response.send_message("**Boop paused the Song.**")
+    except:
+        await args.response.send_message("Boop is not playing any Song. Use '/play_song' command to play a song")
+
+
+@cmd.command(name = "resume",description = "Prompts Boop to resume the paused song",guild = discord.Object(id = 1149408094711980172))
+async def resume(args):
+    global guilds
+    vClient = guilds[args.guild.name]
+    await args.channel.typing()
+    try:
+        if vClient.is_paused():
+            vClient.resume()
+            await args.response.send_message("**Boop has resumed the song.**")
+    except:
+        await args.response.send_message("Boop has not paused any song.")
+
+
+@cmd.command(name = "stop",description = "Prompts Boop to stop the song",guild = discord.Object(id = 1149408094711980172))
+async def stop(args):
+    global guilds
+    vClient = guilds[args.guild.name]   
+    await args.channel.typing()
+    try:
+        vClient.stop()
+        await args.response.send_message("**Boop has stopped playing the song.**")
+    except:
+        await args.response.send_message("Boop is not playing any song.")
+            
 
 #Bot Startup
 
